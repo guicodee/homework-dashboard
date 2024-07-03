96cdf7ab26d9d6463024fd148ec04ef65841f463
guicodee <guilhermedfgo12@gmail.com>
2024-07-03 11:27:23 -0300

    feat: add toaster success on submit form

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
 import { HomeworkProvider } from '@/context/Homework'
+import { Toaster } from './components/ui/toaster'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HomeworkProvider>
       <App />
+      <Toaster />
     </HomeworkProvider>
   </React.StrictMode>,
 )
