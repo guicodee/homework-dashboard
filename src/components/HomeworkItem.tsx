7ce9e27a8e8212d38017afcf2c4e6d4578e492dc
guicodee <guilhermedfgo12@gmail.com>
2024-08-07 14:42:14 -0300

    feat: add the onclick function done homework on the finish button

--- src/components/HomeworkItem.tsx
+++ src/components/HomeworkItem.tsx
@@ -1,4 +1,4 @@
-import { Check, Trash } from "lucide-react";
+import { Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { useContext } from "react";
@@ -10,10 +10,11 @@ interface HomeworkItemProps {
   title: string
   description: string
   priority: "low" | "medium" | "high"
+  done: boolean
 }
 
-export function HomeworkItem({id, title, description, priority}: HomeworkItemProps) {
-  const { deletedHomeworks } = useContext(HomeworkContext);
+export function HomeworkItem({id, title, description, priority, done}: HomeworkItemProps) {
+  const { deletedHomeworks, toggleHomeworkIsDone } = useContext(HomeworkContext);
   const { toast } = useToast();
 
   function handleDeleteHomework() {
@@ -27,25 +28,41 @@ export function HomeworkItem({id, title, description, priority}: HomeworkItemPro
     }, 2000);
   }
 
+  function handleDoneHomework() {
+    setTimeout(() => {
+      toggleHomeworkIsDone(id);
+      toast({
+        variant: "success",
+        description: 'Tarefa finalizada com sucesso.',
+        duration: 2000,
+      });
+    }, 2000);
+  }
+
   return (
-    <div className="border mb-8 p-4 rounded-md">
+    <div className={`border mb-8 p-4 rounded-md ${done ? 'bg-zinc-800/50' : ''}`}>
       <div className="flex justify-between mb-8">
         <div>
-          <h2 className="text-xl antialiased font-medium mb-2">{title}</h2>
-          <span className="text-base antialiased text-zinc-200">{description}</span>
+          <h2 className={`text-xl antialiased font-medium mb-2 ${done ? 'line-through text-muted-foreground' : 'text-zinc-200'}`}>{title}</h2>
+          <span className={`text-base antialiased ${done ? 'line-through text-zinc-400 text-muted-foreground' : 'text-zinc-200'}`}>{description}</span>
         </div>
         <div className="flex flex-col">
           <Label className="text-sm font-semibold text-zinc-300 mb-2">Status</Label>
-          <span className={`font-medium text-sm px-3 rounded-full ${priority === 'low' && 'bg-green-700 text-green-200'} ${priority === 'medium' && 'bg-yellow-700 text-yellow-200'} ${priority === 'high' && 'bg-red-700 text-red-100'}`}>{priority}</span>
+          <div className={`font-medium text-sm px-3 rounded-full ${priority === 'low' && 'bg-green-700 text-green-200'} ${priority === 'medium' && 'bg-yellow-700 text-yellow-200'} ${priority === 'high' && 'bg-red-700 text-red-100'} ${done ? 'line-through bg-zinc-700/40' : ''}`}>
+            <span>{priority}</span>
+          </div>
         </div>
       </div>
       <div className="flex items-center gap-4">
         <Button onClick={handleDeleteHomework} variant={"destructive"}>
           <Trash className="w-5 h-5" />
         </Button>
-        <Button className="flex items-center gap-3 font-semibold" variant={"success"}>
-          <Check className="w-5 h-5" />
-          Concluído
+        <Button onClick={handleDoneHomework} className="flex items-center gap-3 font-semibold" variant={done ? 'default' : 'success'}>
+          {!done ? (
+            <span>Concluído</span>
+          ) : (
+            <span>Pendente</span>
+          )}
         </Button>
       </div>
     </div>
